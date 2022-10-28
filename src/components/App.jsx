d0cfcde95552a7821ebab14cd14935ba2f919613
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-10-28 19:08:44 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import { GetLinkInfo } from "../utils/service";
 
 const App = () => {
   const linkId = window.location.hash.substring(1);
   const [link, setLink] = useState("");
-  GetLinkInfo(linkId)
-    .then((response) => {
-      setLink(response.value[0].Value);
-    })
-    .catch((error) => {
-      setDisplayText("Destination does not exist! 😶");
-    });
+  useEffect(() => {
+    GetLinkInfo(linkId)
+      .then((response) => {
+        setLink(response.value[0].Value);
+      })
+      .catch((error) => {
+        console.log("Destination does not exist! 😶");
+      });
+  }, []);
 
   return (
     <>
       {link ? (
         <video
           id="vid1"
-          class="azuremediaplayer amp-default-skin"
-          autoplay
+          className="azuremediaplayer amp-default-skin"
+          autoPlay
           controls
           width="100%"
-          poster="poster.jpg"
+          poster="https://cdn.bipul.in/bipul.in/images/meta.png"
           data-setup='{"nativeControlsForTouch": false}'
         >
           <source src={link} type="application/vnd.ms-sstr+xml" />
-          <p class="amp-no-js">
+          <p className="amp-no-js">
             To view this video please enable JavaScript, and consider upgrading
             to a web browser that supports HTML5 video
           </p>
