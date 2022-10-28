ffd6d39abbb1f574f902f4551d06d795ee498dfa
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-10-28 19:54:30 +0530

    Code added.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
-import styles from "./App.module.scss";
 import { GetLinkInfo } from "../utils/service";
+import { AzureMP } from "react-azure-mp";
+
+const appStyle = {
+  fontFamily: "Consolas",
+}
 
 const App = () => {
   const linkId = window.location.hash.substring(1);
-  const [link, setLink] = useState("");
+  const [srcData, setSrcData] = useState("");
   useEffect(() => {
     GetLinkInfo(linkId)
       .then((response) => {
-        setLink(response.value[0].Value);
+        const sourceData = [
+          {
+            src: response.value[0].Value,
+            type: "application/vnd.ms-sstr+xml",
+          }
+        ]
+        setSrcData(sourceData);
       })
       .catch((error) => {
         console.log("Destination does not exist! 😶");
@@ -16,29 +26,18 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      {link ? (
-        <video
-          id="vid1"
-          className="azuremediaplayer amp-default-skin"
-          autoPlay
-          controls
-          width="100%"
-          poster="https://cdn.bipul.in/bipul.in/images/meta.png"
-          data-setup='{"nativeControlsForTouch": false}'
-        >
-          <source src={link} type="application/vnd.ms-sstr+xml" />
-          <p className="amp-no-js">
-            To view this video please enable JavaScript, and consider upgrading
-            to a web browser that supports HTML5 video
-          </p>
-        </video>
+    <div style={appStyle}>
+      {srcData ? (
+        <AzureMP
+          skin="amp-flush"
+          src={srcData}
+        />
       ) : (
         <div>
           <h1>LiveStream unavailable.</h1>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
